fe0a62f1a5647ce4fecc3c26cb07eefed705ab07
nickwn12 <nickwoel-nogueira2023@u.northwestern.edu>
2023-04-23 18:59:51 -0500

    Added UI in terminal

--- song.py
+++
@@ -1,9 +0,0 @@
-class Song:
-    def __init__(self, name: str, genre: str, bpm: int, key: str, mood: list, energy: str, year: int):
-        self.name = name
-        self.genre = genre
-        self.bpm = bpm
-        self.key = key
-        self.mood = mood
-        self.energy = energy
-        self.year = year
