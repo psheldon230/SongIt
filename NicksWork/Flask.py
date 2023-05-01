7aa4f6b438801a3dec012aff13b85d02d44278bf
Nicholas Woel-Nogueira <61762704+nickwn12@users.noreply.github.com>
2023-05-01 14:49:03 -0500

    Flask nick (#4)
    * Update Before Working on Main
    
    * MyFlaskWebsiteWorkds
    
    * Lines now look good
    
    * Done

--- NicksWork/Flask.py
+++ NicksWork/Flask.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, jsonify, request
 import pickle as pkl
+from spotifyImages import SPOTIFYINTERFACE
 from songsInterface import SONGSINTERFACE
+import os
+
 app = Flask(__name__)
 
 songs = pkl.load(open("NicksWork/songsDictListPickle.pkl", 'rb'))
@@ -9,23 +12,83 @@ songs = pkl.load(open("NicksWork/songsDictListPickle.pkl", 'rb'))
 @app.route("/")
 def home():
     songs = pkl.load(open("NicksWork/songsDictDictPickle.pkl", 'rb'))
-    return render_template("home.html", songs=songs)
+    return render_template("fancy.html", songs=songs)
 
 
 @app.route('/process-data', methods=['POST'])
 def process_data():
     data = request.get_json()
-    songs = pkl.load(open("NicksWork/songsPickle.pkl", 'rb'))
-    SI = SONGSINTERFACE(songs)
+    songs_file = "NicksWork/songsPickle.pkl"
+    si_file = "NicksWork/SIPickle.pkl"
+
+    # Check if pickle file for SI exists
+    if os.path.isfile(si_file):
+        # Load SI from the pickle file
+        SI = pkl.load(open(si_file, 'rb'))
+    else:
+        # Load songs from the pickle file
+        songs = pkl.load(open(songs_file, 'rb'))
+
+        # Create new instance of SONGSINTERFACE class
+        SI = SONGSINTERFACE(songs)
+
+        # Save SI to new pickle file
+        with open(si_file, 'wb') as f:
+            pkl.dump(SI, f)
     curPrompt = SI.returnPromptWithList(songList=data)
-    curResponce = SI.returnGBTResponce(2000)
-    return {'message': curResponce}
+    # curResponce = SI.returnGBTResponce(2000)
+    return {'message': curPrompt}
+    # return {'message': curResponce}
 
 
 @app.route("/about")
 def about():
     return render_template("about.html")
     return "Hello, World!"
+
+
+@app.route("/process-album-cover-data",  methods=['POST'])
+def processAlbumCoverData():
+    SPI = SPOTIFYINTERFACE()
+    songList = request.get_json()
+    songList.reverse()
+    print(songList)
+    albumCovers = []
+
+    baseAlbumCover = SPI.searchAlbumCover300("Changes Shrek 2")
+    for i in range(5):
+        albumCovers.append(baseAlbumCover)
+
+    for i, searchStr in enumerate(songList):
+        albumCovers[i] = SPI.searchAlbumCover300(searchStr)
+
+    songs_file = "NicksWork/songsPickle.pkl"
+    si_file = "NicksWork/SIPickle.pkl"
+
+    # Check if pickle file for SI exists
+    if os.path.isfile(si_file):
+        # Load SI from the pickle file
+        SI = pkl.load(open(si_file, 'rb'))
+    else:
+        # Load songs from the pickle file
+        songs = pkl.load(open(songs_file, 'rb'))
+
+        # Create new instance of SONGSINTERFACE class
+        SI = SONGSINTERFACE(songs)
+
+        # Save SI to new pickle file
+        with open(si_file, 'wb') as f:
+            pkl.dump(SI, f)
+
+    SI.selectLastSelectedSongsFromList(songList)
+    greetings = SI.lastSelectedSongs
+    curSongDict = {}
+    for song in greetings:
+        curSongDict[song.songArtistName] = vars(song)
+    
+    
+    return {'message': albumCovers, "songsDict": curSongDict}
+
 
 
 if __name__ == "__main__":
--- NicksWork/static/js/app.js
+++ NicksWork/static/js/app.js
@@ -10,19 +10,113 @@ $(document).ready(function () {
     }
     console.log(selectedValues);
     var responce = sendDataToServer(selectedValues);
-    console.log("nick" + responce);
+    $("#promptText").text("Loading Please Wait");
   });
+
+  $("#selectSongs").change(function () {
+    // your code here
+    const selectEl = document.getElementById("selectSongs");
+    const selectedValues = [];
+    for (let i = 0; i < selectEl.options.length; i++) {
+      if (selectEl.options[i].selected) {
+        selectedValues.push(selectEl.options[i].value);
+      }
+    }
+    sendSelectedAlbumsToServer(selectedValues);
+  });
+
+  function setupGraph() {
+    const data = [
+      { x: 10, y: 20 },
+      { x: 20, y: 40 },
+      { x: 30, y: 60 },
+      { x: 40, y: 80 },
+      { x: 50, y: 100 },
+    ];
+
+    const margin = { top: 20, right: 20, bottom: 50, left: 50 };
+    const width = 500 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    const svg = d3
+      .select("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom);
+
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.x)])
+      .range([margin.left, width - margin.right]);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.y)])
+      .range([height - margin.bottom, margin.top]);
+
+    const xAxis = d3.axisBottom(xScale);
+
+    const yAxis = d3.axisLeft(yScale);
+
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height - margin.bottom})`)
+      .call(xAxis);
+
+    svg
+      .append("g")
+      .attr("transform", `translate(${margin.left}, 0)`)
+      .call(yAxis);
+
+    svg
+      .selectAll(".dot")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("class", "dot")
+      .attr("cx", (d) => xScale(d.x))
+      .attr("cy", (d) => yScale(d.y))
+      .attr("r", 5)
+      .style("fill", "steelblue");
+  }
+  // setupGraph();
+
 });
 function formatTextForHTML(text) {
   // Replace newline characters with HTML line breaks
   // const formattedText = text.replace(/\n/g, "<br>");
   // const formattedText = text
-  var formattedText = text
-  return formattedText
+  var formattedText = text;
+  return formattedText;
+
 
   // Replace consecutive whitespace characters with non-breaking spaces
   return formattedText.replace(/[\s]+/g, "&nbsp;");
 }
+
+function sendSelectedAlbumsToServer(data) {
+  $.ajax({
+    url: "/process-album-cover-data",
+    method: "POST",
+    data: JSON.stringify(data),
+    contentType: "application/json",
+    success: function (response) {
+      console.log("Data sent successfully");
+      // console.log(response.message);
+      console.log(response);
+      imageUrls = response.message;
+      const boxes = document.querySelectorAll(".containerBottom .box");
+      boxes.forEach((box, index) => {
+        box.style.backgroundImage = `url(${imageUrls[index]})`;
+      });
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log("Error sending data");
+      $("#promptText").text("Error sending data");
+      console.log(textStatus);
+    },
+  });
+}
+
 
 function sendDataToServer(data) {
   $.ajax({
@@ -32,12 +126,15 @@ function sendDataToServer(data) {
     contentType: "application/json",
     success: function (response) {
       console.log("Data sent successfully");
-      console.log(response.message)
+      console.log(response.message);
+
 
       $("#promptText").text(response.message);
     },
     error: function (jqXHR, textStatus, errorThrown) {
       console.log("Error sending data");
+      $("#promptText").text("Error sending data");
+
       console.log(textStatus);
     },
   });
